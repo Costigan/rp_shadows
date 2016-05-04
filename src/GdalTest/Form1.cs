7d895955cb0b46f4150f8db4519a873cab042e71
Mark Shirley <mark.h.shirley@nasa.gov>
2016-05-03 18:11:26 -0700

    Added code to test reading geotiff files using GDAL.  It works.  I don't know how to interpret the info, though.

---
+++ src/GdalTest/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gdal = OSGeo.GDAL.Gdal;
+using Ogr = OSGeo.OGR.Ogr;
+
+namespace GdalTest
+{
+    public partial class Form1 : Form
+    {
+        string sample_diff = @"C:\git\rp_shadows\data\synthetic-lunar-patch.tif";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GdalConfiguration.ConfigureGdal();
+            var dataset = Gdal.Open(sample_diff, OSGeo.GDAL.Access.GA_ReadOnly);
+            if (dataset == null)
+            {
+                Console.WriteLine(@"couldn't open file");
+                return;
+            }
+            Console.WriteLine(@"Size=[{0},{1}]", dataset.RasterXSize, dataset.RasterYSize);
+            var geoTransform = new double[6];
+            dataset.GetGeoTransform(geoTransform);
+            Console.WriteLine(@"Origin = [{0},{1}]", geoTransform[0], geoTransform[3]);
+            Console.WriteLine(@"Pixel size=[{0},{1}]", geoTransform[1], geoTransform[5]);
+
+
+            foreach (var v in geoTransform)
+                Console.WriteLine(v);
+
+            if (dataset.RasterCount < 1)
+                throw new Exception(@"No bands");
+
+            var band = dataset.GetRasterBand(1);
+            int blockX, blockY;
+            band.GetBlockSize(out blockX, out blockY);
+            Console.WriteLine(@"Block size=[{0},{1}]", blockX, blockY);
+            Console.WriteLine(Gdal.GetDataTypeName(band.DataType));
+        }
+    }
+}
